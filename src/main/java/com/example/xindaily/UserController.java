6fa52ee9cf5beae995d2936d5e49c07cdfe6eba9
honghogn11 <2993400893@qq.com>
2023-12-11 10:49:48 +0800

    服务端- Android端测试

--- src/main/java/com/example/xindaily/UserController.java
+++ src/main/java/com/example/xindaily/UserController.java
@@ -5,6 +5,7 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
@@ -16,18 +17,20 @@ public class UserController {
     @Autowired
     private JdbcTemplate jdbcTemplate;
 
-    @RequestMapping(value = "/getUsers",method = RequestMethod.GET)
+    @RequestMapping(value = "/getUsers",method = RequestMethod.POST)
     @ResponseBody
-    public String getUsers(String name,String passwd){
+    public String getUsers(@RequestParam("uname") String uname){
 
-        String sql = "select * from user";
+        String sql = "select upasswd from user where uname = "+uname;
         List<Map<String,Object>> list = jdbcTemplate.queryForList(sql);
+        String result =list.get(0).get("uname").toString();
+        return result;
 
-        for (Map<String, Object> stringObjectMap : list) {
-            if (stringObjectMap.containsValue(name)) {
-                return "success";
-            }
-        }
-        return "login failed";
+//        for (Map<String, Object> stringObjectMap : list) {
+////            if (stringObjectMap.containsValue(name)) {
+////                return "success";
+////            }
+//        }
+//        return "login failed";
     }
 }
